fb36fe85499bf33d93ef0b291814174a71946b8d
Seth Near <seth.j.near@gmail.com>
2020-07-04 19:05:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+
+attr_accessor :s1, :s2, :s3
+
+def initialize(s1, s2, s3)
+  @s1 = s1
+  @s2 = s2
+  @s3 = s3
+end
+
+
+
+def kind
+  case self
+  when (@s1 == 0) || (@s2 == 0) || (@s3 == 0)
+    raise TriangleError
+  when ((@s1 + @s2) <= @s3) || ((@s1 + @s3) <= @s2) || ((@s2 + @s3) <= @s1)
+    raise TriangleError
+  when (@s1 == @s2) && (@s2 == @s3)
+    kind = :equilateral
+  when ((@s1 == @s2) && (@s1 != @s3)) || ((@s1 == @s3) && (@s1 != @s2)) || ((@s2 == @s3) && (@s2 != @s1))
+    kind = :isosceles
+  when (@s1 != @s2) && (@s2 != @s3)
+    kind = :scalene
+  end
+  return kind
+end
+
+class TriangleError < StandardError
+  # triangle error code
+end
+
+
 end
