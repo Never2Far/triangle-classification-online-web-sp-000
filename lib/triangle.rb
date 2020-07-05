1d51dc27be77e440d498f712960ea16ed129e01e
Seth Near <seth.j.near@gmail.com>
2020-07-05 18:31:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -33,59 +33,6 @@ def kind
 end
 
 class TriangleError < StandardError
-  # def message1
-  #   "INVALID TRIANGLE: All sides of a triangle must have a length greater than 0!"
-  # end
-  #
-  # def message2
-  #   "INVALID TRIANGLE: The sum of the lengths of two sides of a triangle must exceed that of the third."
-  # end
-
 end
 
-
 end
-
-
-
-
-# if (@s1 == 0) || (@s2 == 0) || (@sides[2] == 0)
-#   begin
-#     raise TriangleError
-#   rescue TriangleError => error
-#     puts error.message1
-#   end
-# elsif ((@s1 + @s2) <= @sides[2]) || ((@s1 + @sides[2]) <= @s2) || ((@s2 + @sides[2]) <= @s1)
-#   begin
-#     raise TriangleError
-#   rescue TriangleError => error
-#     puts error.message2
-#   end
-# elsif (@s1 == @s2) && (@s2 == @sides[2])
-#   return :equilateral
-# elsif ((@s1 == @s2) && (@s1 != @sides[2])) || ((@s1 == @sides[2]) && (@s1 != @s2)) || ((@s2 == @sides[2]) && (@s2 != @s1))
-#   return :isosceles
-# elsif (@s1 != @s2) && (@s2 != @sides[2])
-#   return :scalene
-# end
-
-# def valid?
-#   # if @sides.any? {|s|  s <= 0 } || @sides.any?
-#   if (@sides[0] <= 0) || (@sides[1] <= 0) || (@sides[2] <= 0)
-#     begin
-#         raise TriangleError
-#       rescue TriangleError => error
-#         puts error.message1
-#       end
-#       return false
-#   elsif ((@sides[0] + @sides[1]) <= @sides[2]) || ((@sides[0] + @sides[2]) <= @sides[1]) || ((@sides[1] + @sides[2]) <= @sides[0])
-#       begin
-#         raise TriangleError
-#       rescue TriangleError => error
-#         puts error.message2
-#       end
-#       return false
-#     else
-#       return true
-#     end
-# end
