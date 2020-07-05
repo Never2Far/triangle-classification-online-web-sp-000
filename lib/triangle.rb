cf50f2671280efe2b57f006c8d1c9b8bb2115661
Seth Near <seth.j.near@gmail.com>
2020-07-05 17:26:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,34 +2,35 @@ class Triangle
 
 attr_accessor :s1, :s2, :s3
 
+
+
 def initialize(s1, s2, s3)
-  @s1 = s1
-  @s2 = s2
-  @s3 = s3
+  @sides = [s1, s2, s3]
 end
 
-
+def valid?
+  if @sides.any? {|s|  (s <= 0) || (s == nil) }
+    begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message1
+      end
+      return false
+  elsif ((@sides[0] + @sides[1]) <= @sides[2]) || ((@sides[0] + @sides[2]) <= @sides[1]) || ((@sides[1] + @sides[2]) <= @sides[0])
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message2
+      end
+      return false
+    else
+      return true
+    end
+end
+  
 
 def kind
-  if (@s1 == 0) || (@s2 == 0) || (@s3 == 0)
-    begin
-      raise TriangleError
-    rescue TriangleError => error
-      puts error.message1
-    end
-  elsif ((@s1 + @s2) <= @s3) || ((@s1 + @s3) <= @s2) || ((@s2 + @s3) <= @s1)
-    begin
-      raise TriangleError
-    rescue TriangleError => error
-      puts error.message2
-    end
-  elsif (@s1 == @s2) && (@s2 == @s3)
-    return :equilateral
-  elsif ((@s1 == @s2) && (@s1 != @s3)) || ((@s1 == @s3) && (@s1 != @s2)) || ((@s2 == @s3) && (@s2 != @s1))
-    return :isosceles
-  elsif (@s1 != @s2) && (@s2 != @s3)
-    return :scalene
-  end
+  return unless !valid?
 end
 
 class TriangleError < StandardError
@@ -45,3 +46,26 @@ end
 
 
 end
+
+
+
+
+# if (@s1 == 0) || (@s2 == 0) || (@s3 == 0) 
+#   begin
+#     raise TriangleError
+#   rescue TriangleError => error
+#     puts error.message1
+#   end
+# elsif ((@s1 + @s2) <= @s3) || ((@s1 + @s3) <= @s2) || ((@s2 + @s3) <= @s1)
+#   begin
+#     raise TriangleError
+#   rescue TriangleError => error
+#     puts error.message2
+#   end
+# elsif (@s1 == @s2) && (@s2 == @s3)
+#   return :equilateral
+# elsif ((@s1 == @s2) && (@s1 != @s3)) || ((@s1 == @s3) && (@s1 != @s2)) || ((@s2 == @s3) && (@s2 != @s1))
+#   return :isosceles
+# elsif (@s1 != @s2) && (@s2 != @s3)
+#   return :scalene
+# end
